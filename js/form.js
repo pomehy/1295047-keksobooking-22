74fb13be31201169719d73b3f51f0a01e562ee0d
pomehy <hlopkov.gleb@mail.ru>
2021-03-09 10:22:16 +0300

    функции синхронизации полей формы

--- js/form.js
+++ js/form.js
@@ -11,3 +11,15 @@ const typeOfHouseForm = adForm.querySelector('#type');
 const priceForm = adForm.querySelector('#price');
 const timeInForm = adForm.querySelector('#timein');
 const timeOutForm = adForm.querySelector('#timeout');
+
+typeOfHouseForm.addEventListener('change', (evt) => {
+  priceForm.placeholder = priceForm.min = TYPE_MIN_PRICES[evt.target.value];
+});
+
+timeOutForm.addEventListener('change', (evt) => {
+  timeInForm.value = evt.target.value;
+});
+
+timeInForm.addEventListener('change', (evt) => {
+  timeOutForm.value = evt.target.value;
+});
