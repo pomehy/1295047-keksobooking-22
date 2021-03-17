a1ef9c9ef6b35d0dfb3601b3de366a5a0d2880b6
pomehy <hlopkov.gleb@mail.ru>
2021-03-17 22:06:01 +0300

    add getData and sendData

---
+++ js/api.js
@@ -0,0 +1,30 @@
+const getData = (url, onSuccess, onFail) => {
+  return fetch(url)
+    .then ((response => {
+      if (response.ok) {
+        return response.json();
+      }
+      onFail('Не удалось загрузить объявления: ' + response.text);
+    }))
+    .then(onSuccess)
+    .catch(onFail);
+};
+
+const sendData = (url, onSuccess, onFail, body) => {
+  return fetch(
+    url,
+    {
+      method: 'POST',
+      body,
+    },
+  )
+    .then((response) => {
+      if (response.ok) {
+        return onSuccess();
+      }
+      onFail();
+    })
+    .catch(onFail);
+};
+
+export { getData, sendData };
